2f9bd57637545026e10ef4c89189fec4bdbfa9e6
Rainwater <jqrvz6@mst.edu>
2014-07-15 14:01:01 -0500

    Basic structure for file

---
+++ carmen_tracker.cpp
@@ -0,0 +1,25 @@
+//Jared Rainwater --- carmen_tracker.cpp
+//Description
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int iterations, num_villains;
+    float key;
+    string file_name, output_name;
+    
+    cout << "Welcome. Please provide the file name: ";
+    cin >> file_name;
+    ifstream fin(file_name.c_str());
+    
+    //get info from file
+    
+    
+    //track villains
+    
+    
+}
