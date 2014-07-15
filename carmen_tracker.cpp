cfc33a42bdd0ba2308c02b32cfaa675cf8d2fb35
Rainwater <jqrvz6@mst.edu>
2014-07-15 14:13:03 -0500

    Read info from the file

--- carmen_tracker.cpp
+++ carmen_tracker.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     int iterations, num_villains;
-    float key;
+    double key;
     string file_name, output_name;
     
     cout << "Welcome. Please provide the file name: ";
@@ -17,7 +17,15 @@ int main()
     ifstream fin(file_name.c_str());
     
     //get info from file
+    fin >> iterations;
+    fin >> key;
+    fin >> num_villains;
+    fin >> output_name;
     
+    cout << iterations << endl;
+    cout << key << endl;
+    cout << num_villains << endl;
+    cout << output_name << endl;
     
     //track villains
     
