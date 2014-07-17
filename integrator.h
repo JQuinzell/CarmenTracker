57cf0b9abd961c1dbd740b32e999c1efd6684f9f
Rainwater <jqrvz6@mst.edu>
2014-07-17 14:06:19 -0500

    Starting integrator class

---
+++ integrator.h
@@ -0,0 +1,17 @@
+//Jared Rainwater --- integrator.h
+
+#ifndef INTEGRATOR_H
+#define INTEGRATOR_H
+
+#include "point3d.h"
+
+class Integrator
+{
+private:
+    Point3D point;
+    double timestep;
+public:
+    
+};
+
+#endif
